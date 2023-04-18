5657ade1353077c3566e8f6d12947c984934db5e
MariaShev777 <mariashevtsova1403@gmail.com>
2023-04-18 23:39:42 +0300

    1-1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
+export type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 function App() {
+
+    const heading1 = 'What to learn'
+    const heading2 = 'What to watch'
+
+
+    const tasks1:TasksType[] = [
+        {id: 1, title: 'JS', isDone: true},
+        {id: 2, title: 'CSS', isDone: false},
+        {id: 3, title: 'HTML', isDone: false}
+    ]
+
+    const tasks2:TasksType[] = [
+        {id: 1, title: 'ReactJS', isDone: false},
+        {id: 2, title: 'Movie', isDone: true},
+        {id: 3, title: 'YouTube', isDone: false},
+        {id: 4, title: 'Comedy', isDone: false}
+    ]
+
+
     return (
-        <div>
-            Naming
-            <Todolist/>
+        <div className='App'>
+            <Todolist tasks={tasks1} heading={heading1}/>
+            <Todolist tasks={tasks2} heading={heading2}/>
         </div>
     )
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import {TasksType} from "./App";
 
-export const Todolist = () => {
+
+type TodolistPropsType = {
+    tasks: TasksType[]
+    heading: string
+}
+
+
+export const Todolist = (props: TodolistPropsType) => {
     return (
         <div>
-            <input/>
-            <button>+</button>
+            <h3>{props.heading}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el => {
+                    return (
+                        <li><input type="checkbox" checked={el.isDone}/><span>{el.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
