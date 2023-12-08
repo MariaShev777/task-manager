fed69083662c974edc84c23e592f87a7f4eaf2b8
MariaShev777 <masha_shevtsova@tut.by>
2023-12-08 18:47:52 +0300

    thunkTryCatch - logout, login

--- src/features/auth/model/auth.reducer.ts
+++ src/features/auth/model/auth.reducer.ts
@@ -33,40 +33,30 @@ const slice = createSlice({
 
 const login = createAppAsyncThunk<{ isLoggedIn: boolean }, LoginParamsType>(`${slice.name}/login`, async (arg, thunkAPI) => {
   const { dispatch, rejectWithValue } = thunkAPI;
-  try {
-    dispatch(appActions.setAppStatus({ status: 'loading' }));
+  return thunkTryCatch(thunkAPI, async () => {
     const res = await authAPI.login(arg);
     if (res.data.resultCode === RESPONSE_RESULT.SUCCESS) {
-      dispatch(appActions.setAppStatus({ status: 'succeeded' }));
       return { isLoggedIn: true };
     } else {
       const isShowAppError = !res.data.fieldsErrors?.length;
       handleServerAppError(res.data, dispatch, isShowAppError);
       return rejectWithValue(res.data);
     }
-  } catch (e) {
-    handleServerNetworkError(e, dispatch);
-    return rejectWithValue(null);
-  }
+  });
 });
 
 const logout = createAppAsyncThunk<{ isLoggedIn: boolean }, void>(`${slice.name}/logout`, async (_, thunkAPI) => {
   const { dispatch, rejectWithValue } = thunkAPI;
-  try {
-    dispatch(appActions.setAppStatus({ status: 'loading' }));
+  return thunkTryCatch(thunkAPI, async () => {
     const res = await authAPI.logout();
     if (res.data.resultCode === RESPONSE_RESULT.SUCCESS) {
       dispatch(todolistsActions.clearTodosData());
-      dispatch(appActions.setAppStatus({ status: 'succeeded' }));
       return { isLoggedIn: false };
     } else {
       handleServerAppError(res.data, dispatch);
       return rejectWithValue(null);
     }
-  } catch (e) {
-    handleServerNetworkError(e, dispatch);
-    return rejectWithValue(null);
-  }
+  });
 });
 
 const initializeApp = createAppAsyncThunk<{ isLoggedIn: boolean }, void>(`${slice.name}/initializeApp`, async (_, thunkAPI) => {
@@ -84,6 +74,5 @@ const initializeApp = createAppAsyncThunk<{ isLoggedIn: boolean }, void>(`${slic
 });
 
 export const authReducer = slice.reducer;
-export const authActions = slice.actions;
 
 export const authThunks = { login, logout, initializeApp };
